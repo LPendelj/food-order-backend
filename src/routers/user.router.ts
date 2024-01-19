0dbc62a2326b6652768d04013211a7398ddf3324
Luka Pendelj <pendeljluka@gmail.com>
2024-01-19 13:54:58 +0100

    Added the possibility to register new users

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -3,6 +3,8 @@ import { sample_users } from '../data';
 import jwt from "jsonwebtoken";
 import asyncHandler from 'express-async-handler'
 import { User, UserModel } from '../models/user.model';
+import { HTTP_BAD_REQUEST } from '../constants/http_status';
+import bcrypt from 'bcryptjs';
 
 const router = Router();
 
@@ -23,16 +25,41 @@ router.post("/login", asyncHandler(
     async (req, res) => {
         const {email, password} = req.body;
         const user = await UserModel.findOne({
-            email, password
+            email
         })
         // stari nacin filtriranja dok nije bilo baze.
         //sample_users.find(user => user.email === email && user.password === password);
     
-        if(user){
+        if(user && (await bcrypt.compare(password,user.password))){
             res.send(generateToken(user))
         } else {
-            res.status(400).send("Username or Password are not valid")
+            res.status(HTTP_BAD_REQUEST).send("Username or Password are not valid")
         }
+    }
+))
+
+router.post("/register", asyncHandler(
+    async (req, res) =>{
+        const { name, email, password, address } = req.body;
+        const user = await UserModel.findOne({email});
+        if(user){
+            res.status(HTTP_BAD_REQUEST).send('User already exists. Please login!')
+            return
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        const newUser: User = {
+            id: '',
+            name,
+            email: email.toLowerCase(),
+            address,
+            isAdmin: false,
+            password: encryptedPassword
+        }
+
+        const dbUser = await UserModel.create(newUser);
+
+        res.send(generateToken(dbUser));
     }
 ))
 
