bc7762202c2fe3d4262d4a058d5b3185545cba14
Luka Pendelj <pendeljluka@gmail.com>
2025-05-10 18:53:19 +0200

    [Server config] Changed to remove the serve of index.html

--- src/server.ts
+++ src/server.ts
@@ -25,10 +25,10 @@ app.use("/api/foods", foodRouter)
 app.use("/api/users", userRouter)
 app.use("/api/orders", orderRouter)
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+// app.use(express.static('public'));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
 
 const port = process.env.PORT || 5000;
 
