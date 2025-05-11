2ef295f5147a4602f00caa39a167c9df424e05cf
Luka Pendelj <pendeljluka@gmail.com>
2025-05-11 16:42:56 +0200

    [Server config] Allowed CORS for the Netlify app

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors(
     {
         credentials: true,
-        origin: ["http://localhost:4200", "https://food-order-backend-w9fd.onrender.com"]
+        origin: ["http://localhost:4200", "https://6820b62d3ed4950008ce983e--foodman-foodorder.netlify.app", "*.netlify.app"]
     }
 ))
 
